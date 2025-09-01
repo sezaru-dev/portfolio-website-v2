f8243d0d6395d665b891087d2240f07309cbecc8
sezaru-dev <cezarbaguhin20@gmail.com>
2025-09-01 10:24:12 +0800

    feat(about-section): create a about section component

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className='wrapper'>
+      <div className='mb-12'>
+        <h2 className='text-xl text-neutral-50/60 mb-4'>ABOUT</h2>
+        <p className='text-3xl lg:text-4xl font-medium max-w-xl'>Driven by Passion</p>
+      </div>
+        <p className='text-xl font-light text-neutral-300 max-w-3xl leading-relaxed'>Hi, I’m Cezar. I’ve always loved the idea of creating <span className='font-semibold'>“inventions” that solve problems—</span> and web development became my way to do it. I’m focused on mastering <span className='font-semibold'>Next.js, React, Tailwind CSS, and TypeScript</span> , while expanding into backend tools like <span className='font-semibold'>NextAuth and MongoDB</span>. For me, every line of code is part of shaping something useful, practical, and innovative.</p>
+    </section>
+  )
+}
