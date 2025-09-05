402c306e59f66a24b1c42e4a24fb58dc2a0afdf9
sezaru-dev <cezarbaguhin20@gmail.com>
2025-09-05 13:34:43 +0800

    feat(badge):  improve the styles of Badge component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,8 +6,9 @@ type BadgeProps = {
 
 export default function Badge({children}: BadgeProps) {
   return (
-    <span className='inline-flex items-center rounded-full px-2.5 py-0.5 text-sm font-medium transition-colors bg-neutral-700'>
+    <span className="inline-flex items-center rounded-full px-3 py-1 text-xs lg:text-sm font-medium bg-neutral-800/80 text-white border border-neutral-700 shadow-sm">
       {children}
     </span>
+
   )
 }
