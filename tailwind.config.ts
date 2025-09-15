49c5cb4d203b834bd62a008f84bc318c6be8fdc8
sezaru-dev <cezarbaguhin20@gmail.com>
2025-09-15 20:56:47 +0800

    remove keyframes and animation extention

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,25 +9,16 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
+        blob: "hsl(var(--blob) / <alpha-value>)",
       },
       fontFamily: {
         sans: ['Poppins', 'ui-sans-serif', 'system-ui'], 
       },
-       keyframes: {
-        wave: {
-          '0%, 100%': { transform: 'translateX(-50%) translateY(0) skewX(0deg) scaleY(1)' },
-          '25%': { transform: 'translateX(-50%) translateY(12px) skewX(-5deg) scaleY(1.05)' },
-          '50%': { transform: 'translateX(-50%) translateY(0) skewX(0deg) scaleY(1)' },
-          '75%': { transform: 'translateX(-50%) translateY(-12px) skewX(5deg) scaleY(0.95)' },
-        },
-      },
-      animation: {
-        wave: 'wave 6s ease-in-out infinite',
-      },
     },
   },
   plugins: [],
+  darkMode: "class", // for next-themes
 };
 export default config;
