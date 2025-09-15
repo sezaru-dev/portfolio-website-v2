edc68746180bbd564006f38b3e7a5248c229c1c5
sezaru-dev <cezarbaguhin20@gmail.com>
2025-09-15 22:07:13 +0800

    create and implement StructuredData for SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,23 @@ import Header from "@/components/sections/Header";
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
 import Skills from "@/components/sections/Skills";
+import StructuredData from "@/components/StructuredData";
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <main>
-        <Hero/>
-        <Skills/>
-        <Projects/>
-        <About/>
-        <Contact/>
-        <Footer/>
-      </main>
-    </div>
+    <>
+      <StructuredData />
+      <div>
+        <Header/>
+        <main>
+          <Hero/>
+          <Skills/>
+          <Projects/>
+          <About/>
+          <Contact/>
+          <Footer/>
+        </main>
+      </div>
+    </>
   );
 }
---
+++ src/components/StructuredData.tsx
@@ -0,0 +1,34 @@
+// components/seo/StructuredData.tsx
+"use client"
+
+import Script from "next/script"
+
+export default function StructuredData() {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Cezarlito O. Baguhin",
+    alternateName: "SezaruDev",
+    url: "https://cezarlitobaguhin.vercel.app",
+    jobTitle: "Frontend Developer",
+    description:
+      "Transforming ideas into interactive web experiences using React, Next.js, and modern web technologies.",
+    sameAs: [
+      "https://github.com/sezaru-dev",
+      "https://linkedin.com/in/cezarlito-baguhin-184748175",
+      "https://www.facebook.com/sezaru20",
+      "https://www.tiktok.com/@sezarudev",
+    ],
+  }
+
+  return (
+    <Script
+      id="ld-json-home"
+      type="application/ld+json"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(structuredData),
+      }}
+    />
+  )
+}
