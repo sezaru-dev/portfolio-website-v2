ac547304dd5fe2590d398aaf40b5698f2bd9d127
sezaru-dev <cezarbaguhin20@gmail.com>
2025-09-15 20:59:44 +0800

    update default theme to dark

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -7,7 +7,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"        // adds class="light" or "dark" on <html>
-      defaultTheme="system"    // use system theme by default
+      defaultTheme="dark"    // use dark theme by default
       enableSystem             // allow system preference
       disableTransitionOnChange // smoother theme switch
     >
