f8baf9042c614b66dc142fdbade6b1df0a3fd764
sezaru-dev <cezarbaguhin20@gmail.com>
2025-09-15 21:11:38 +0800

    improve styles for light mode and dark mode

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,7 +6,7 @@ type BadgeProps = {
 
 export default function Badge({children}: BadgeProps) {
   return (
-    <span className="inline-flex items-center rounded-full px-3 py-1 text-xs lg:text-sm font-medium bg-neutral-800/80 text-white border border-neutral-700 shadow-sm">
+    <span className="inline-flex items-center rounded-full px-3 py-1 text-xs lg:text-sm font-medium bg-neutral-500 dark:bg-neutral-800/80 text-white dark:border dark:border-neutral-700 shadow-sm">
       {children}
     </span>
 
